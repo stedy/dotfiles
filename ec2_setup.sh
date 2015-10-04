2781143b4c30f7e757ef09ce95516ce21d7b1f4c
stedy <zstednic@fhcrc.org>
2015-10-04 19:16:29 +0000

    updated from ubuntu 12.04 to 14.04

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -1,6 +1,6 @@
 #start with GPG key for latest R
 
-echo "deb http://lib.stat.cmu.edu/R/CRAN/bin/linux/ubuntu precise/" | sudo tee -a /etc/apt/sources.list
+echo "deb http://lib.stat.cmu.edu/R/CRAN/bin/linux/ubuntu trusty/" | sudo tee -a /etc/apt/sources.list
 gpg --keyserver pgp.mit.edu --recv-key 51716619E084DAB9
 gpg -a --export 51716619E084DAB9 > cran.asc
 sudo apt-key add cran.asc
@@ -13,4 +13,12 @@ sudo apt-get install -y -qq r-base r-base-dev
 
 sudo apt-get install -y -qq python-dev python-scipy python-numpy python-matplotlib python-pandas python-nose python-sympy python-scikits.learn
 
+sudo apt-get install libxml2-dev libxslt1-dev
 
+sudo apt-get install -qq python-pip
+
+sudo pip install BeautifulSoup
+
+sudo apt-get install littler
+
+sudo r -e 'install.packages(c("dplyr","XML", "tm"), repos = "http://cran.case.edu")' 
