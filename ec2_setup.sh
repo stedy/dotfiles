6ccc4f326940ce3bf954ffa53cf68df6e8eb3d4b
stedy <zstednic@fhcrc.org>
2015-10-04 20:53:47 +0000

    add rstudio server config

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -1,5 +1,4 @@
 #start with GPG key for latest R
-
 echo "deb http://lib.stat.cmu.edu/R/CRAN/bin/linux/ubuntu trusty/" | sudo tee -a /etc/apt/sources.list
 gpg --keyserver pgp.mit.edu --recv-key 51716619E084DAB9
 gpg -a --export 51716619E084DAB9 > cran.asc
@@ -9,8 +8,7 @@ sudo rm cran.asc
 sudo apt-get update -qq
 sudo apt-get install -y -qq ubuntu-dev-tools gdebi-core libapparmor1 psmisc libtool autoconf automake uuid-dev git octave
 
-sudo apt-get install -y -qq r-base r-base-dev
-
+#python dependent installs
 sudo apt-get install -y -qq python-dev python-scipy python-numpy python-matplotlib python-pandas python-nose python-sympy python-scikits.learn
 
 sudo apt-get install libxml2-dev libxslt1-dev
@@ -19,9 +17,14 @@ sudo apt-get install -qq python-pip
 
 sudo pip install BeautifulSoup
 
+#R dependent installs
+sudo apt-get install -y -qq r-base r-base-dev
 sudo apt-get install littler
 
-sudo r -e 'install.packages(c("dplyr","XML", "tm"), repos = "http://cran.case.edu")' 
+sudo r -e 'install.packages(c("dplyr","XML", "tm"), repos = "http://cran.case.edu")'
+
+wget https://download2.rstudio.org/rstudio-server-0.99.484-amd64.deb
+sudo gdebi rstudio-server-0.99.484-amd64.deb
 
 mkdir ~/.vim
 mkdir ~/.vim/backup
